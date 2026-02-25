860826c194465c0595614a82ee77a19e48e76df9
CSamu7 <samuel.pdg@hotmail.com>
2026-02-24 22:10:08 -0600

    Se añadio la carpeta de integration testing.

---
+++ Testing/IntegrationTesting/DailyTasksTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+
+namespace Testing.IntegrationTesting
+{
+    public class DailyTasksTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private WebApplicationFactory<Program> _factory;
+        public DailyTasksTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+        [Fact]
+        public async Task DailyTasksTodayNotReturnDailyTasksFromPreviousDays()
+        {
+            var testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync();
+        }
+    }
+}
---
+++ Testing/TestApplicationFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Time.Testing;
+
+namespace Testing
+{
+    public class TestApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            DateTimeOffset today = new DateTime(2026, 2, 26);
+            TimeProvider testDate = new FakeTimeProvider(today);
+            
+            builder.ConfigureTestServices(services =>
+            {
+                services.RemoveAll<TimeProvider>();
+                services.AddSingleton<TimeProvider>();
+            });
+        }
+    }
+}
+
